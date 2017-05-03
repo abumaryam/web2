6d1873524984d69e679c7ff885986780b09babaa
kapuas <mqor@s.bloq.ro>
2017-05-03 13:53:26 +0700

    Belajar include dan require

---
+++ belajar/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Penggabungan</title>
+</head>
+<body>
+	<h1>Judul Halaman</h1>
+	<?php 
+		include 'tabel.php';
+		require 'gambar.php';
+	 ?>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab tempore, alias error aperiam deserunt architecto minima corporis, dolorum quis culpa itaque labore tenetur, molestiae repellendus non quod facilis quisquam, aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores magnam laboriosam, rem temporibus non, nemo illum possimus doloremque totam nulla, dolorem architecto eos. Rem, eveniet, dolorum. Minus, quaerat. Esse, consectetur.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi modi quasi, unde eligendi ratione laudantium similique nam cumque, placeat iste labore dolorem iusto ipsum illo numquam voluptatibus, itaque quam. Rem!</p>
+	<?php 
+		include_once 'tabel.php';
+	 ?>
+</body>
+</html>
---
+++ belajar/tabel.php
@@ -0,0 +1,12 @@
+<table border="1">
+	<tr>
+		<td>No</td>
+		<td>Nim</td>
+		<td>Nama</td>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>X1111</td>
+		<td>Dian</td>
+	</tr>
+</table>
