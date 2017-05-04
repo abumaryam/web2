39c60261877feabecc383d7ec7f35bc23414caa9
kapuas <mqor@s.bloq.ro>
2017-05-04 13:29:48 +0700

    template halaman depan

--- administrator/template/main.php
+++ administrator/template/main.php
@@ -1,6 +1,10 @@
 <?php 
+include_once 'header.php';
+include_once 'sidebar.php';
+?>
+<h1 class="page-header">Ini Judul</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ipsa maxime voluptatum asperiores! Necessitatibus odio amet, suscipit at nam deserunt nemo maiores accusantium asperiores quis eligendi ullam cum, ea aliquid!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus in dolorem fugit nulla officiis quasi, numquam, voluptatum fugiat, quibusdam atque quisquam facere hic. Quos laudantium perspiciatis hic itaque. Earum, quo!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi molestiae neque quam ducimus animi dolore magni dolor impedit, tenetur cum, facilis nihil aperiam consequatur asperiores voluptate fugiat rerum deserunt nulla.</p>
 
-
-
-
+<?php
+include_once 'footer.php';
  ?>
